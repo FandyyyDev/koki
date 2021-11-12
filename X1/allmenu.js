b77800aa26a4180c2e407010b1eb89230dd10b46
Fbotzz <nazril.afandi1603@gmail.com>
2021-11-13 06:11:21 +0700

    y

---
+++ X1/allmenu.js
@@ -0,0 +1,45 @@
+const allmenu = (tz, nama, namabot, prefix, day, yy, thisDay, bulan, wib, wita, wit) => { 
+	return `❲ *${namabot}* ❳
+${tz} *OWNER* : ${nama}
+${tz} *BOTNAME* : ${namabot}
+${tz} *PREFIX* : *${prefix}* 
+${tz} *TGGAL* : ${day}
+${tz} *HARI KE* : ${yy}
+${tz} *HARI* : ${thisDay}
+${tz} *BULAN* : ${bulan}
+${tz} *WIB* : ${wib}
+${tz} *WITA* : ${wita}
+${tz} *WIT* : ${wit}
+
+❲ *GROUPMENU* ❳
+${tz} *${prefix}antilink* <on/off>
+${tz} *${prefix}antivirtext* <on/off>
+${tz} *${prefix}delete* <reply.txt>
+${tz} *${prefix}hidetag* <text>
+${tz} *${prefix}tagall* <text>
+${tz} *${prefix}linkgc* 
+${tz} *${prefix}listadmin*
+${tz} *${prefix}setdesc* <text>
+${tz} *${prefix}group* <buka/tutup>
+${tz} *${prefix}setname* <text>
+${tz} *${prefix}listonline*
+${tz} *${prefix}add* <62xxx>
+${tz} *${prefix}kick* <@tag/reply>
+
+❲ *OWNERMENU* ❳
+${tz} *${prefix}return* <kode>
+${tz} *${prefix}setppbot* <reply.img>
+${tz} *${prefix}join* <link>
+${tz} *${prefix}settz* <kode>
+${tz} *${prefix}setprefix* <text>
+${tz} *${prefix}bcgc* <text>
+${tz} *${prefix}bc* <text>
+
+❲ *INFO LAIN* ❳
+${tz} Jika bot tidak merespon, mungkin bot sedang off
+${tz} bot tidak menyimpan riwayat foto/media
+${tz} Silahkan beri waktu 5 detik penggunaan per fitur agar tidak menyebabkan spam
+${tz} Jika menemukan bug/err silahkan hubungi owner
+`
+}
+exports.allmenu = allmenu
---
+++ X1/help.js
@@ -0,0 +1,25 @@
+const help = (tz, nama, namabot, prefix, day, yy, thisDay, bulan, wib, wita, wit) => { 
+	return `❲ *${namabot}* ❳
+${tz} *OWNER* : ${nama}
+${tz} *BOTNAME* : ${namabot}
+${tz} *PREFIX* : *${prefix}* 
+${tz} *TGGAL* : ${day}
+${tz} *HARI KE* : ${yy}
+${tz} *HARI* : ${thisDay}
+${tz} *BULAN* : ${bulan}
+${tz} *WIB* : ${wib}
+${tz} *WITA* : ${wita}
+${tz} *WIT* : ${wit}
+
+❲ *RANDOM TEXT* ❳
+${tz} *${prefix}ownermenu*
+${tz} *${prefix}gamemenu*
+
+❲ *INFO LAIN* ❳
+${tz} Jika bot tidak merespon, mungkin bot sedang off
+${tz} bot tidak menyimpan riwayat foto/media
+${tz} Silahkan beri waktu 5 detik penggunaan per fitur agar tidak menyebabkan spam
+${tz} Jika menemukan bug/err silahkan hubungi owner
+`
+}
+exports.help = help
